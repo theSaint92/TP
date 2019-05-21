dd590d216faf9b000652505b6353b3cec103f025
Adrian Grzelak <grzadr@gmail.com>
2019-05-21 20:31:21 +0200

    Added interface ICasinoService + small changes

---
+++ Casino/Casino/CasinoService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class CasinoService : ICasinoService
+    {
+        private ICasinoRepository repository;
+
+        /**
+         * Constructors
+         */
+        public CasinoService(ICasinoRepository r)
+        {
+            repository = r;
+        }
+
+        public int AddClient(string name, string surname, DateTime dateOfBirth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddGame(string gameName, string gameDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParticipation(int clientId, int playGameId, DateTime startTime, TimeSpan duration, double profit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddPlayGame(int gameId, DateTime startTime, TimeSpan duration, double minimumDeposit, double entryFee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeClientData(int client, string newName, string newSurname, DateTime dateOfBirth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ClientsToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GamesToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ParticipationsToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PlayGamesToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RepositoryToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Game> SelectClientGames(int client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Participation> SelectClientParticipations(int client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlayGame> SelectClientPlayGames(int client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlayGame> SelectGamePlayGames(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game SelectLeastProfitableGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game SelectMostProfitableGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Casino/Casino/ICasinoService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.ObjectModel; //ObservableCollection
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    interface ICasinoService //IDataService
+    {
+
+        /**
+         * Functions returning DataContext elements as string.
+         */ 
+        string ClientsToString();
+        string GamesToString();
+        string PlayGamesToString();
+        string ParticipationsToString();
+
+        /**
+         * Function returning whole repository as a pretty string
+         */
+        string RepositoryToString();
+
+        /**
+         * Adding elements to repository
+         */ 
+        int AddClient( string name , string surname , DateTime dateOfBirth );
+        int AddGame( string gameName , string gameDescription );
+        int AddPlayGame( int gameId, DateTime startTime, TimeSpan duration, double minimumDeposit, double entryFee);
+        void AddParticipation(int clientId, int playGameId, DateTime startTime, TimeSpan duration, double profit);
+
+        /**
+         * Changeing client data in case we need. Maybe he changed name, surname or gender... dunno bro
+         */ 
+        void ChangeClientData(int client, string newName, string newSurname, DateTime dateOfBirth);
+
+        /**
+         * Returning list of participations of specified player
+         */ 
+        List<Participation> SelectClientParticipations(int client);
+
+        /**
+         * Returning list of PlayGames for selected game.
+         */ 
+        List<PlayGame> SelectGamePlayGames(int gameId);
+
+        /**
+         * Returing most profitable game from Casino point of view.
+         */
+        Game SelectMostProfitableGame();
+        
+        /**
+         * Returing least profitable game from Casino point of view.
+         */
+        Game SelectLeastProfitableGame();
+        
+        /**
+         * Selecting Games that client played.
+         */
+        List<Game> SelectClientGames(int client);
+        
+        /**
+         * Selecting PlayGames that client participated in.
+         */
+        List<PlayGame> SelectClientPlayGames(int client);
+    }
+}
