b2e634b28a73d884f221bf024d84332a0f68c624
Adrian Grzelak <grzadr@gmail.com>
2019-05-23 01:15:26 +0200

    Added subscribers to CollectionChanged event of ObservableCollections

--- Casino/Casino/CasinoContext.cs
+++ Casino/Casino/CasinoContext.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic; //List & Dictionary
+﻿using System;
+using System.Collections.Generic; //List & Dictionary
 using System.Collections.ObjectModel; //ObservableCollection
+using System.Collections.Specialized;
 
 namespace Casino
 {
@@ -16,7 +18,38 @@ namespace Casino
             GameList = new Dictionary<int, Game>();
             PlayGameList = new ObservableCollection<PlayGame>();
             ParticipationList = new ObservableCollection<Participation>();
-        
+
+            //Adding subscribers
+            PlayGameList.CollectionChanged += this.OnPlayGameCollectionChanged;
+            ParticipationList.CollectionChanged += this.OnParticipationCollectionChanged;
+        }
+
+        /**
+         * Subscriber to PlayGameList OnCollectionChanged
+         */ 
+        public void OnPlayGameCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+                foreach(PlayGame p in e.NewItems)
+                    Console.WriteLine("Added PlayGame element with id " + p.Id + " into DataContext.");
+                
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+                foreach (PlayGame p in e.OldItems)
+                    Console.WriteLine("Removed PlayGame element with id " + p.Id + " from DataContext.");
+        }
+
+        /**
+         * Subscriber to ParticipationList OnCollectionChanged
+         */
+        public void OnParticipationCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+                foreach (Participation p in e.NewItems)
+                    Console.WriteLine("Added Participation element with id " + p.Id + " into DataContext.");
+
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+                foreach (Participation p in e.OldItems)
+                    Console.WriteLine("Removed Participation element with id " + p.Id + " from DataContext.");
         }
     }
 }
