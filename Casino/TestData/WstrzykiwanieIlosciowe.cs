d657656eec394de9eb30ceb77d269350e995ec49
48730 <48730@DESKTOP-F1JAOHI>
2019-05-23 13:40:03 +0200

    WstrzykiwanieIlosciowe z użyciem FAKER API

---
+++ Casino/TestData/WstrzykiwanieIlosciowe.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class WstrzykiwanieIlosciowe : IWstrzykiwanieDanych
+    {
+
+        private int numberOfClients;
+        private int numberOfGames;
+        private int numberOfPlayGames;
+
+        public WstrzykiwanieIlosciowe(int clients, int games, int playGames)
+        {
+            numberOfClients = clients;
+            numberOfGames = games;
+            numberOfPlayGames = playGames;
+
+        }
+
+
+
+        public void WypelnijCasinoContext(ref CasinoContext context)
+        {
+
+            //add clients
+            for (int i = 0; i < numberOfClients; i++)
+            {
+                int Id = i;
+                string Name = Faker.Name.First();
+                string Surname = Faker.Name.Last();
+                DateTime DateOfBirth = new DateTime((2000 + i / 365) % 5000, i % 12 + 1, i % 28 + 1);
+                context.ClientList.Add(new Client(Id, Name, Surname, DateOfBirth));
+            }
+
+
+            //add games
+            for (int i = 0; i < numberOfGames; i++)
+            {
+                int Id = i;
+                string GameName = string.Concat("gameName", i);
+                string GameDescription = string.Concat("gameDescription", i);
+                context.GameList.Add(i, new Game(Id, GameName, GameDescription));
+
+            }
+
+
+            //add playGames
+            for (int i = 0; i < numberOfPlayGames; i++)
+            {
+                for (int j = 0; j < numberOfPlayGames; j++)
+                {
+                    PlayGame playGame = new PlayGame(i, context.GameList[i], new DateTime((2000 + i / 365), i % 12 + 1, i % 28 + 1), new TimeSpan(i + 20), i + 50, i + 20);
+                    context.PlayGameList.Add(playGame);
+                }
+            }
+
+
+            //add Participation
+            for (int i = 0; i < context.PlayGameList.Count / 2; i++)
+            {
+                Client client = context.ClientList[i % context.ClientList.Count];
+                PlayGame playGame = context.PlayGameList[i];
+                Participation participation = new Participation(i + 10, client, playGame, new DateTime((2000 + i / 365) % 5000, i % 12 + 1, i % 28 + 1), new TimeSpan(i + 10), i + 100);
+                context.ParticipationList.Add(participation);
+            }
+
+
+
+
+        }
+    }
+}
+
---
+++ Casino/UnitTestProject/WstrzykiwanieIloscioweTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Casino;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class WstrzykiwanieIloscioweTest
+    {
+        
+ 
+
+        [TestMethod]
+        public void WstrzykiwanieIloscioweTestPoprawnosc()
+        {
+            ICasinoRepository repository = new CasinoRepository(new WstrzykiwanieIlosciowe(10,5,5));
+
+            Assert.AreEqual(10, repository.GetAllClients().Count());
+            Assert.AreEqual(5, repository.GetAllGames().Count());
+            Assert.AreEqual(25, repository.GetAllPlayGames().Count());
+            Assert.AreEqual(12, repository.GetAllParticipations().Count());
+
+        }
+
+       
+    }
+}
