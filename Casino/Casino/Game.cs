7167ccef1432014dd32f978cfe7f0470b7103330
Adrian Grzelak <grzadr@gmail.com>
2019-06-24 12:45:51 +0200

    Proper implementation of INotifyPropertyChanged for basic model classes

--- Casino/Casino/Game.cs
+++ Casino/Casino/Game.cs
@@ -1,16 +1,56 @@
-﻿namespace Casino
+﻿using System.ComponentModel;
+
+namespace Casino
 {
-    public class Game //Katalog
+    public class Game : INotifyPropertyChanged //Katalog
     {
-        public int Id { get; }
-        public string GameName { get; set; }
-        public string GameDescription { get; set; }
+        private int _id;
+        private string _gameName;
+        private string _gameDescription;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string GameName
+        {
+            get { return _gameName; }
+            set
+            {
+                _gameName = value;
+                OnPropertyChanged("GameName");
+            }
+        }
+        public string GameDescription
+        {
+            get { return _gameDescription; }
+            set
+            {
+                _gameDescription = value;
+                OnPropertyChanged("GameDescription");
+            }
+        }
 
         public Game(int id, string gameName, string gameDescription)
         {
-            this.Id = id;
-            this.GameName = gameName;
-            this.GameDescription = gameDescription;
+            this._id = id;
+            this._gameName = gameName;
+            this._gameDescription = gameDescription;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
 
         public override string ToString()
--- Casino/Casino/Participation.cs
+++ Casino/Casino/Participation.cs
@@ -1,25 +1,91 @@
 ﻿using System; //DateTime & TimeSpan
+using System.ComponentModel;
 
 namespace Casino
 {
-    public class Participation //Zdarzenie
+    public class Participation : INotifyPropertyChanged //Zdarzenie
     {
-        public int Id { get; set; }
-        public Client Participator { get; set; }
-        public PlayGame PlayedGame { get; set; }
-        public DateTime StartTime { get; set; }
-        public TimeSpan Duration { get; set; }
-        public double Profit { get; set; }
+        private int _id;
+        private Client _participator;
+        private PlayGame _playedGame;
+        private DateTime _startTime;
+        private TimeSpan _duration;
+        private double _profit;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public Client Participator
+        {
+            get { return _participator; }
+            set
+            {
+                _participator = value;
+                OnPropertyChanged("Participator");
+            }
+        }
+        public PlayGame PlayedGame
+        {
+            get { return _playedGame; }
+            set
+            {
+                _playedGame = value;
+                OnPropertyChanged("PlayedGame");
+            }
+        }
+        public DateTime StartTime
+        {
+            get { return _startTime;  }
+            set
+            {
+                _startTime = value;
+                OnPropertyChanged("StarTime");
+            }
+        }
+        public TimeSpan Duration
+        {
+            get { return _duration; }
+            set
+            {
+                _duration = value;
+                OnPropertyChanged("Duration");
+            }
+        }
+        public double Profit
+        {
+            get { return _profit; }
+            set
+            {
+                _profit = value;
+                OnPropertyChanged("Profit");
+            }
+        }
 
         public Participation(int id, Client participator, PlayGame playedGame, DateTime startTime, TimeSpan duration, double profit)
         {
-            this.Id = id;
-            this.Participator = participator;
-            this.PlayedGame = playedGame;
-            this.StartTime = startTime;
-            this.Duration = duration;
-            this.Profit = profit;
+            this._id = id;
+            this._participator = participator;
+            this._playedGame = playedGame;
+            this._startTime = startTime;
+            this._duration = duration;
+            this._profit = profit;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
 
         public override string ToString()
         {
